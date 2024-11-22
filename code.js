34b9ad20ca6177625bf20e55232cf894441081b0
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-11-21 21:13:02 -0700

    Update code.js

--- code.js
+++ code.js
@@ -6,10 +6,10 @@ function tsp_hk(distance_matrix) {
 
     //Helper function to find shortest distance with Help-Karp
     //FST = Find Shortest Tour  
-    //'unvisitedCities' is used as a bitmask where it shows if it is visited (1) or not visited (0)
-    //currentCity is the current city that is being processed
-    function FST(unvisitedCities, currentCity) { 
-        const citiesKey = `${unvisitedCities}-${currentCity}`; // Key to store the subset result 
+    //'visitedCities' is used as a bitmask where it shows if it is visited (1) or not visited (0)
+    //'currentCity' is the current city that is being processed
+    function FST(visitedCities, currentCity) { 
+        const citiesKey = `${visitedCities}-${currentCity}`; // Key to store the subset result 
 
         //Check if subset has been solved
         if (memo.has(citiesKey)) {
@@ -17,8 +17,8 @@ function tsp_hk(distance_matrix) {
         }
 
         //Base case: If all cities have been visited. return distance to last.
-        //'unvisitedCities === (1 << numCities) - 1' means all bits are set (all cities have been visited) 
-        if (unvisitedCities === (1 << numCities) - 1) { 
+        //'visitedCities === (1 << numCities) - 1' means all bits are set (all cities have been visited) 
+        if (visitedCities === (1 << numCities) - 1) { 
             return distance_matrix[currentCity][numCities - 1] || 0; // return 0 if no connection exists
         } 
 
@@ -27,9 +27,9 @@ function tsp_hk(distance_matrix) {
         //Recursive case: explore next cities
         for (let nextCity = 0; nextCity < numCities; nextCity++) { 
             //If city has not been visited
-            if (!(unvisitedCities & (1 << nextCity))) {
+            if (!(visitedCities & (1 << nextCity))) {
                 //Add 'nextCity' to the visited set by setting it with a corresponding bit. 
-                const remainingCities = unvisitedCities | (1 << nextCity); 
+                const remainingCities = visitedCities | (1 << nextCity); 
                 //Will calculate the cost of the visiting 'nextCity' and continue the search 
                 const cost = distance_matrix[currentCity][nextCity] + FST(remainingCities, nextCity); 
                 minCost = Math.min(minCost, cost);
